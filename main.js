bd1066197febc0729d5fd75d1bdfc47ccb0dc2ce
Ericdid <ericdidom@gmail.com>
2023-07-19 17:25:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// - chiedere la distanza da percorrere
+const Distance = parseInt(prompt(`Distanza Da Percorrere`));
+console.log("distanza da percorrere : " + Distance + "km");
+
+// - chiedere l età
+const Userage = parseInt(prompt(`inserisci la tua età`));
+console.log("età del passeggero : " + Userage);
+
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+const Price = 0.21;
+
+typeof Distance;
