4568998b8fee12e6fd5aa411f3b372cf90ea4fbd
Ericdid <ericdidom@gmail.com>
2023-07-19 18:54:00 +0200

    add.discounts/max.decimals

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// (vislizza numeri fino ai decimali)
+function toFixedIfNecessary(value, dp) {
+  return +parseFloat(value).toFixed(dp);
+}
+
 // - chiedere la distanza da percorrere
 const Distance = parseInt(prompt(`Distanza Da Percorrere`));
 console.log("distanza da percorrere : " + Distance + "km");
@@ -9,9 +14,25 @@ console.log("età del passeggero : " + Userage);
 // - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 const Price = 0.21;
 
-let number = Distance * 0.21;
-console.log("prezzo tratta : " + number + "€");
+let totalprice = Distance * Price;
+console.log("prezzo tratta : " + toFixedIfNecessary(totalprice, 2) + "€");
 
 // - va applicato uno sconto del 20% per i minorenni
 if (Userage < 18) {
+  discountund = totalprice - (totalprice * 20) / 100;
+  console.log(
+    "prezzo tratta con sconto minori : " +
+      toFixedIfNecessary(discountund, 2) +
+      "€"
+  );
+}
+
+// va applicato uno sconto del 40% per gli over 65.
+if (Userage > 65) {
+  discountovr = totalprice - (totalprice * 40) / 100;
+  console.log(
+    "prezzo tratta con sconto anziani : " +
+      toFixedIfNecessary(discountovr, 2) +
+      "€"
+  );
 }
